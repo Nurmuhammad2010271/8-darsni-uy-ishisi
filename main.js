c43e94f7091c7a25f06e0f6e68a21a8b9628dd9b
Nurmuhammad2010271 <938630762a@gmail.com>
2025-02-22 09:32:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let mevalar = ["banan","malina","nok"];
+console.log(mevalar);
+mevalar.forEach(mevalar1 =>{
+    console.log(`${mevalar1} n harif ishtirok etgan harif`);
+});
+
+let mevalar2 = ["uzum","olma","gilos","olcha"];
+console.log(mevalar2);
+mevalar2.forEach(mevalar2 =>{
+    console.log(`${mevalar2} n harif ishtirok etmagan harif`);
+});
